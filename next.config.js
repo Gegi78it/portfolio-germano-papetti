7e2d709e94235cf0534d028673a321eb9a729b55
Germano Papetti <germanopapetti@Mac-mini-di-Germano.local>
2026-01-08 11:16:50 +0100

    Netlify static export fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // importante per siti statici su Netlify
   output: "export",
-
-  // importante per evitare problemi con next/image in export statico
   images: {
     unoptimized: true,
   },
