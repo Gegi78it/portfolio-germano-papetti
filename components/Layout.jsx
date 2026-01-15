2696ec09346e4b24094a0547f943dbf3aad086c0
Il Tuo Nome <germano.papetti@gmail.com>
2026-01-15 15:01:28 +0100

    Add Nav to layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
+import Nav from "./Nav";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
+      <Nav />
       <main className="min-h-screen pt-[100px]">
         {children}
       </main>
